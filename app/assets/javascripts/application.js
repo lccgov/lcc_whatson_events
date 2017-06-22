b66d759d864481d7a0bcb9e6e6535b54d712021d
Travis CI <developer@leeds.gov.uk>
2017-06-22 10:25:17 +0100

    udated to latest templates and front end toolkit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -143,6 +143,24 @@
             $('.rbEvents input:radio').attr('checked', false);
             $('.rbVenues input:radio').attr('checked', true);
         });
+
+
+             jQuery('ul.date-list').each(function () {
+      var LiN = jQuery(this).find('li').length;
+      if (LiN > 3) {
+      jQuery('li', this).eq(2).nextAll().hide().addClass('toggleable');
+      jQuery(this).append('<a class="plusMinus">Show more...</a>');
+      }
+      });
+      jQuery('ul.date-list').on('click', '.plusMinus', function () {
+      if (jQuery(this).hasClass('active')) {
+      jQuery(this).text('Show more...').removeClass('active');
+      } else {
+      jQuery(this).text('Show less...').addClass('active');
+      }
+      jQuery(this).siblings('li.toggleable').slideToggle();
+      });
+
     });
 })(jQuery);
 
